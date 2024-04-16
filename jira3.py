57730d9b35aa6094eeadc60ddc4116c5d677db47
SaiKruthin <88674966+SaiKruthin@users.noreply.github.com>
2024-04-16 22:49:49 +0530

    Delete jira3.py

--- jira3.py
+++
@@ -1,4 +0,0 @@
-search_results = jira.search_issues('project=PROJECT AND assignee=currentUser()')
-for issue in search_results:
-    print("Issue Key:", issue.key)
-    print("Issue Summary:", issue.fields.summary)
