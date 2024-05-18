f96bb8ad4bdeab0bf947002952f4314d838b0ac8
SaiKruthin <88674966+SaiKruthin@users.noreply.github.com>
2024-05-18 21:50:47 +0530

    Delete extrat.py

--- extrat.py
+++
@@ -1,6 +0,0 @@
-extract_between = lambda text, start, end: text[text.find(start) + len(start):text.find(end, text.find(start) + len(start))] if text.find(start) != -1 and text.find(end, text.find(start) + len(start)) != -1 else None
-text = "Here is some <tag>content</tag> in the text."
-start = "<tag>"
-end = "</tag>"
-content = extract_between(text, start, end)
-print(content)  # Output: content
