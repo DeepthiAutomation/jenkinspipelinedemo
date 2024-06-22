06186abae04efc0be7a5468f2f164796195aa4af
SaiKruthin <88674966+SaiKruthin@users.noreply.github.com>
2024-06-23 00:42:54 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-# Initialize the Dash app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
-server = app.server  # Expose the server variable for gunicorn
