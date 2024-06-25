03611b3915267331dde6c9d13c08c08811abd478
SaiKruthin <88674966+SaiKruthin@users.noreply.github.com>
2024-06-25 23:50:32 +0530

    Update footer.py

--- footer.py
+++ footer.py
@@ -3,3 +3,4 @@
                style={'textAlign': 'center', 'padding': '20px', 'color': 'white'})
     ], style={'backgroundColor': '#007bff', 'position': 'fixed', 'bottom': '0', 'width': '100%'})
 ])
+style={'fontSize': '18px', 'fontWeight': 'bold'}),
